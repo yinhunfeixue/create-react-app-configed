0b4894d300925265f4d49fcdcc6545c3cd5dc619
徐俊杰 <>
2018-12-03 19:11:08 +0800

    修复，连续多次hash相同但是参数不同时，异步加载的组件仍然会新建对象的问题（不是走willreceiveprops，而是走didmount)

--- src/components/Async.js
+++ src/components/Async.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
 const URL = require('url');
 
+const CACHE = new Map();
 
 export default function async(importComponent) {
+  let key = importComponent.toString();
+  if (CACHE.has(key)) {
+    return CACHE.get(key);
+  }
   /**
    * 类方法
    */
@@ -29,5 +34,7 @@ export default function async(importComponent) {
       return C ? <C {...this.props} query={URL.parse(this.props.location.search, true).query} /> : null;
     }
   }
+
+  CACHE.set(key, AsyncComponent);
   return AsyncComponent;
 }
